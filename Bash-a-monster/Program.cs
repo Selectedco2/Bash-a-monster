9ffe7bd6ee673f4c3e4f3b48cbab9f885707406a
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-09-06 12:19:51 +0200

    Initial commit

---
+++ Bash-a-monster/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Character Warlock = new Character();
+Enemy Abomination = new Enemy();
+
+Console.WriteLine("You encounter an abomination and a shiver runs down your spine.");
+Console.WriteLine("He rushes towards you and initiates combat");
+Console.WriteLine("-----------------------------------------------------------------");
+
+Console.WriteLine($"{Warlock.name} has {Warlock.health} health");
+Console.WriteLine($"{Abomination.name} has {Abomination.health} health");
+Console.WriteLine("Would you like to attack, make peace... or get freaky..? [A) Fireball] [B) Peace]");
+
+string firstStrike = Console.ReadLine().ToLower();
+
+if (firstStrike == "a")
+{
+    Console.WriteLine("You quickly lunge at the abomination before he can think");
+}
+else if (firstStrike == "b")
+{
+    Console.WriteLine("");
+}
+
+Console.ReadKey();
+
+while (Warlock.health > 0 && Abomination.health > 0)
+{
+    
+}
+
+
---
+++ Bash-a-monster/class.cs
@@ -0,0 +1,16 @@
+ Random random = new Random();
+ Random randomDamage = new Random(10);
+public class Character
+{
+    public int health = 100;
+    public int Fireball = 20;
+    public int Enthral = (10 + )
+    public string name = "Warlock";
+}
+
+public class Enemy
+{
+    public int health = 100;
+    public int physicaldamage = 15;
+    public string name = "Abomination";
+}
